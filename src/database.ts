bd220fa7d00b25a5e9ba929e8393ec916ae5155f
fcmiranda <fecmbr@gmail.com>
2025-07-05 11:29:43 -0300

    feat: normalize database

--- src/database.ts
+++ src/database.ts
@@ -6,11 +6,22 @@ database.serialize(() => {
     database.run(`CREATE TABLE clients(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        balance DECIMAL(15,2) DEFAULT 0.00
+        email TEXT NOT NULL UNIQUE
     )`);
 
-    database.run(`INSERT INTO clients(name, email, balance) VALUES(?, ?, 0)`, ['TEST', 'TEST@TEST.com']);
+    database.run(`CREATE TABLE accounts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        balance DECIMAL(15,2) DEFAULT 0.00,
+        client_id INTEGER,
+        FOREIGN KEY(client_id) REFERENCES clients(id)
+    )`);
+
+    database.run(`INSERT INTO clients(name, email) VALUES(?, ?)`, ['TEST', 'TEST@TEST.com'], function (err) {
+        if (err) {
+            return console.error(err.message);
+        }
+        database.run(`INSERT INTO accounts(client_id, balance) VALUES(?, ?)`, [this.lastID, 0]);
+    });
 });
 
 export default database;
